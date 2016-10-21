dbcf3148f604f4555aadfea07439b9149243635b
steven-mcmaster <markdado.phone@gmail.com>
2016-10-21 19:45:27 -0400

    Created by Coding Ground Client

---
+++ timestable/timestable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void) {
+  int start, end, num;
+  start = 1;
+  end = 10;
+  
+  for(num = start; num < end; ++num) {
+    printf("  %d", num);
+  }
+  puts("");
+
+  for(num = start; num < end; ++num) {
+    printf(" %d\n", num);
+  }
+  printf("debuggggg")
+
+}
